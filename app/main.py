b8572cce564a5473cab3b14e2383130742b46aec
Andy Wood <mongolsamurai@gmail.com>
2021-06-24 23:38:26 -0700

    Add stub routes to main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "The app is running!! :)"
+
+@app.route('/cube/all')
+def list_cubes():
+    return "The app is running!! :)\nlist_cubes()"
+
+@app.route('/cube/new')
+def new_cube():
+    return "The app is running!! :)\nnew_cube()"
+
+@app.route('/cube/<uuid:cube_id>')
+def get_cube(cube_uuid):
+    return "The app is running!! :)\nget_cube()"
+
+@app.route('/cube/<uuid:cube_id>/edit')
+def edit_cube(cube_uuid):
+    return "The app is running!! :)\nedit_cube()"
+
+@app.route('/cube/<uuid:cube_id>/delete')
+def delete_cube(cube_uuid):
+    return "The app is running!! :)\ndelete_cube()"
